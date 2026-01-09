ded72f31a14346fcb8ff93d76f813033c7b5c007
Vaibhav Adhikari <vaad4590@gmail.com>
2026-01-09 12:39:48 +0530

    commit

---
+++ DAY1/string.py
@@ -0,0 +1,20 @@
+course = "python for 'beginners'"
+course1 = 'python for "beginners"'
+course2 = '''
+how well do you know me 
+how are you doing
+
+hahahhaha new ship drowned
+
+'''
+print(course)
+print(course1)
+print(course2)
+print(course1[3]) # here we printedd the 3rd index of the string
+print(course[-1]) #in python negative index refere to the reverse indexing of any string
+print(course[1:3]) #it returns the values from index 1 to index 2
+adha_course = course[1:10]#cretes a new varibale for string from index1 to 9
+
+print(adha_course)
+name=('jannat')
+print(name[1:-1])#this calls the elemenst from index 1 to -2 in reverse
---
+++ DAY1/stringFormatting.py
@@ -0,0 +1,8 @@
+first = "rama"
+last = "laxman"
+#string formatting makes it easier for us to visualise the sting
+msg = first+ " and " + last + " went to vanvasa. "# here as you can see for longer sentences it will creta a hectic job to add so many concatenating symbols so inestead of this we use string formatting to ease our work
+msg1 = f'{first} and {last} went to vanavasa.' # now adding placeholders insetad of concatenating made our work so much easier
+
+print(msg)
+print(msg1)
