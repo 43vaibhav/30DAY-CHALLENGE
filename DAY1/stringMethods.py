b6b6ad5b4561aad4b4c470a1923bda0b7b6ab563
Vaibhav Adhikari <vaad4590@gmail.com>
2026-01-10 11:32:41 +0530

    commit

---
+++ DAY1/stringMethods.py
@@ -0,0 +1,7 @@
+course = 'TERI MUMMY KI JAI bhootni ke'
+print(len(course))
+print(course.upper())
+print(course.lower())
+print(course.find('t'))
+print(course.replace('bhootni ke', 'mere bhai'))
+print(course)
