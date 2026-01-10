1c024569be3f54a1fae597f93ec0cc577cb14275
Vaibhav Adhikari <vaad4590@gmail.com>
2026-01-10 12:13:37 +0530

    commit

--- DAY1/stringMethods.py
+++ DAY1/stringMethods.py
@@ -4,4 +4,6 @@ print(course.upper())
 print(course.lower())
 print(course.find('t'))
 print(course.replace('bhootni ke', 'mere bhai'))
+print(course.title()) # makes the first letter capital of each word in the string
+print('bhootni ' in course) # in operator checks for the condition
 print(course)
