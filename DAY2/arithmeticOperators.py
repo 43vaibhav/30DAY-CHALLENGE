287dda74f6308aa5db8d9555b4a6fa13eaa5d739
VAIBHVA ADHIKARI <155263128+43vaibhav@users.noreply.github.com>
2026-01-11 18:54:10 +0530

    Add comments to arithmetic operations in Python
    Added comments to clarify the output of arithmetic operations.

--- DAY2/arithmeticOperators.py
+++ DAY2/arithmeticOperators.py
@@ -3,8 +3,8 @@ y = 3
 print(x + y)
 print(x - y)
 print(x * y)
-print( x / y)
-print(x // y)
-print( x ** y)
-print(x % y)
+print( x / y)#this gives float vallue
+print(x // y)# this is done or getting the int value of division
+print( x ** y)#this gives the power
+print(x % y)#this gives the remainder
 
